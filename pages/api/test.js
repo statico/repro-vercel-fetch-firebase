8c48eaa77153bcaeb95503e05a20a16f85e79cce
Ian Langworth <ian@langworth.com>
2023-02-25 18:52:58 -0800

    Try adding the crypto bit

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,3 +1,5 @@
+import { createHmac } from "crypto";
+
 export const config = {
   api: {
     bodyParser: false,
@@ -15,6 +17,24 @@ export default async function (req, res) {
       resolve(buffer);
     });
   });
+
+  // Check the signature per https://api.slack.com/authentication/verifying-requests-from-slack
+  const signingSecret = "sekrit";
+  if (process.env.NODE_ENV === "production" && !signingSecret) {
+    throw new Error("SLACK_SIGNING_SECRET is required in production");
+  }
+  if (signingSecret) {
+    const hmac = createHmac("sha256", signingSecret);
+    hmac.update(
+      ["v0", req.headers["x-slack-request-timestamp"], rawBody].join(":")
+    );
+    const sig = "v0=" + hmac.digest("hex");
+    if (sig !== req.headers["x-slack-signature"]) {
+      // throw new Error("Invalid signature");
+      console.log("invalid signature");
+    }
+    console.log("signature ok");
+  }
 
   const res2 = await fetch(
     "https://hacker-news.firebaseio.com/v0/item/30167605.json"
